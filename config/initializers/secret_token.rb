e87c1c89465283d85a201873612045848ab34624
Pedro Torrao <pedro.luis.torrao@gmail.com>
2011-06-09 12:17:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoFastBikes::Application.config.secret_token = 'be3652c37b71c5e6a0fd8fb67fcccc1475cccf958b1e074270548f84a9da460a7717045dfb175bdcbe2661207904959e39864a976ef2c3c40e938f3ff3d4afb2'
