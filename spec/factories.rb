5ee9217a0086f02bba56f7e38c7ff15955772179
Pedro Torrao <pedro.luis.torrao@gmail.com>
2011-06-15 17:53:19 +0200

    User model with password, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Mickey Mouse"
+  user.email                 "mickey.mouse@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
